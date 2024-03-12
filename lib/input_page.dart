834a81286cea0f2cead316f4017111dd9671f837
= <=>
2024-03-12 12:58:46 -0400

    using gesture widgets

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,8 @@
+import 'package:bmiapp/icon_content.dart';
+import 'package:bmiapp/reusable_card.dart';
+import 'package:bmiapp/variables.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -10,7 +14,7 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(
+        title: const Center(
           child: Text(
             'BMI CALCULATOR',
             style: TextStyle(
@@ -19,10 +23,44 @@ class _InputPageState extends State<InputPage> {
           ),
         ),
       ),
-      body: Center(
-        child: Text(
-          'Body Text',
-        ),
+      body: Column(
+        children: <Widget>[
+          const Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child:
+                      IconContent(label: "MALE", icon: FontAwesomeIcons.mars),
+                ),
+                Expanded(
+                  child: IconContent(
+                      label: "FEMALE", icon: FontAwesomeIcons.venus),
+                ),
+              ],
+            ),
+          ),
+          const Expanded(
+            child: ReusableCard(color: primaryBackgroundColor),
+          ),
+          const Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(color: primaryBackgroundColor),
+                ),
+                Expanded(
+                  child: ReusableCard(color: primaryBackgroundColor),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: bottomContainerBackgroundColor,
+            margin: const EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: bottomContainerHeight,
+          ),
+        ],
       ),
     );
   }
