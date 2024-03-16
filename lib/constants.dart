da00864f2665bc5616540e40ae8003eb4a89dd0c
= <=>
2024-03-16 17:27:01 -0400

    adding the const files

---
+++ lib/constants.dart
@@ -0,0 +1,16 @@
+import "package:flutter/material.dart";
+
+const labelStyle = TextStyle(
+  fontSize: 18.0,
+  color: textColor,
+);
+
+const kNumberStyle = TextStyle(fontSize: 50, fontWeight: FontWeight.bold);
+
+const double bottomContainerHeight = 80.0;
+const Color activeCardColor = Color(0xFF1D1E33);
+// const Color activeCardColor = Colors.white;
+const Color inactiveCardColor = Color(0xFF111328);
+const Color primaryBackgroundColor = Color(0xFF0A0E21);
+const Color bottomContainerBackgroundColor = Color(0xFFEB1555);
+const Color textColor = Color(0xFF8D8E98);
